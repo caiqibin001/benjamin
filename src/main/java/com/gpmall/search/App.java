4f8080786a53ffa0fc22bc3278ddfae97c525494
caiqibin <543036863@qq.com>
2019-08-09 00:08:36 +0800

    Revert "commit test"
    This reverts commit 9c96de309a4853b583fa294348da2b78998658df.

--- src/main/java/com/gpmall/search/App.java
+++ src/main/java/com/gpmall/search/App.java
@@ -6,7 +6,8 @@ package com.gpmall.search;
  */
 public class App {
 
-    public static void main( String[] args ){
+    public static void main( String[] args )
+    {
         System.out.println("test");
         System.out.println( "Hello World!" );
     }

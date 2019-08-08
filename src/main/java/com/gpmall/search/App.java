3b1d8212993953c09403b329e0986c66418ca07c
caiqibin <543036863@qq.com>
2019-08-08 22:27:55 +0800

    commit test

---
+++ src/main/java/com/gpmall/search/App.java
@@ -0,0 +1,14 @@
+package com.gpmall.search;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args )
+    {
+        System.out.println("test");
+        System.out.println( "Hello World!" );
+    }
+}
